77c3d19fe15604235a466f4efe23eda0f5cd6a19
Ethan <elnair122@gmail.com>
2022-09-26 15:16:18 -0400

    eventsAndRoomsAreSetUp

--- server/index.js
+++ server/index.js
@@ -1,21 +1,36 @@
-const express = require ('express')
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-const PORT = 3000
+const PORT = 3000;
 const server = app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
-const socket = require('socket.io');
+const socket = require("socket.io");
 const serverSocket = socket(server);
 
-serverSocket.on('connection', (socket) => {
- console.log(`Connection from client ${socket.id}`);
- setInterval(() => {
-   const time = (new Date()).toLocaleString()
-   socket.emit('time-change', time)
- }, 1000)
+serverSocket.on("connection", (socket) => {
+  console.log(`Connection from client ${socket.id}`);
+
+  setInterval(() => {
+    const time = new Date().toLocaleString();
+    socket.emit("time-change", time);
+  }, 1000);
+
+  socket.on("send-message", (message, room) => {
+    if (room === "") {
+      socket.broadcast.emit("receive-message", message);
+    } else {
+      socket.to(room).emit("receive-message", message);
+    }
+  });
+
+  socket.on("join-room", (room, cb) => {
+    console.log(room);
+    socket.join(room);
+    cb(`Joined ${room}`);
+  });
 });
